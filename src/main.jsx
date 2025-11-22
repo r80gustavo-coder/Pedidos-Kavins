f5b41d31e6fa7615a25a75b0058b6c90701ef759
r80gustavo-coder <r80gustavo@gmail.com>
2025-11-22 19:35:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// Aqui você importaria seu arquivo de CSS global/Tailwind (Ex: import './index.css')
+import './index.css' // LINHA ADICIONADA/DESCOMENTADA PARA CARREGAR O TAILWIND
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
